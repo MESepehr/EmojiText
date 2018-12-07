eb5f56eee50d27950d0b4262b1778f3e3964c4d9
M.Ebrahim <ebrahimsepehr@gmail.com>
2018-12-07 10:19:08 +0330

    Text and emoji

--- RandomText/randomText.js
+++ RandomText/randomText.js
@@ -1,23 +1,75 @@
-var randomText = function(PIXI,W,H)
+var randomText = function(PIXI,onSizeUpdated=()=>{})
 {
     var Sprite = PIXI.Sprite ;
     var Text = PIXI.Text ;
     var me = this.me = new Sprite();
-    this.init = function(textField='')
+    var x = 0 ;
+
+    var lastText = '' ;
+    var emojiesReady = false ;
+
+    const emojiList = [
+        "./RandomText/img/smile.png",
+        "./RandomText/img/dollar.png"
+    ] ;
+    const emojiChars = [
+        '☺',
+        '$'
+    ]
+
+    PIXI.loader
+    .add(
+        emojiList
+    )
+    .load(emojiesLoaded);
+
+    function emojiesLoaded()
     {
+        emojiesReady = true ;
+        init(lastText);
+    }
+
+    init = this.init = function(text='')
+    {
+        lastText = text ;
+
         me.removeChildren();
-        var x = 0 ; 
-        for(var i = 0 ; i<textField.length ; i++)
+        x = 0 ; 
+        for(var i = 0 ; i<text.length ; i++)
         {
+            var currentText = text.charAt(i) ;
+            if(emojiesReady)
+            {
+                var foundedEmojiIndex = emojiChars.indexOf(currentText);
+                if(foundedEmojiIndex!==-1)
+                {
+                    var emoj = new Sprite(PIXI.loader.resources[emojiList[foundedEmojiIndex]].texture);
+                    me.addChild(emoj);
+                    emoj.x = x ;
+                    x+=emoj.width ;
+                    continue;
+                }
+            }
             var style = new PIXI.TextStyle({
                 fontSize: 16,
                 fill: "white",
             });
-            var textField = new Text(textField.charAt(i),style);
+
+            var textField = new Text(currentText,style);
             me.addChild(textField);
-            alert("textField.charAt(i) : "+textField.charAt(i));
-            x+=textField.width ;
+            textField.x = x ;
+            //Calculate text size here
+            var textMetrics = PIXI.TextMetrics.measureText(currentText, style);
+            x+=textMetrics.width ;
         }
+
+        onSizeUpdated();
+    }
+
+
+    this.getWidth = function()
+    {
+        return x ;
     }
     return this ;
 }
